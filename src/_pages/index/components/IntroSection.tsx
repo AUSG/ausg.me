ab2aa5bcc0594e7815c3cf07887c0304a63dadb6
Coldot <41678750+Coldot@users.noreply.github.com>
2026-06-01 02:17:43 +0900

    fix: lint error in IntroSection

--- src/_pages/index/components/IntroSection.tsx
+++ src/_pages/index/components/IntroSection.tsx
@@ -92,7 +92,9 @@ export default function IntroSection() {
                   <div className="relative whitespace-nowrap rounded-full bg-white px-8 py-3.5 text-[23px] font-bold text-primary shadow-[-10px_10px_0px_rgba(0,0,0,0.3)]">
                     {isApplyPeriod
                       ? `${generation}기 모집 중!`
-                      : `${RECRUITMENT_CONFIG.openDate.month() + 1}월 중 모집 예정!`}
+                      : `${
+                          RECRUITMENT_CONFIG.openDate.month() + 1
+                        }월 중 모집 예정!`}
                     <div className="absolute -bottom-2.5 left-1/2 h-0 w-0 -translate-x-1/2 border-l-[10px] border-r-[10px] border-t-[10px] border-l-transparent border-r-transparent border-t-white" />
                   </div>
                 </div>
@@ -169,7 +171,9 @@ export default function IntroSection() {
                 <div className="relative whitespace-nowrap rounded-full bg-white px-6 py-2.5 text-[18px] font-bold text-primary shadow-[-6px_6px_0px_rgba(0,0,0,0.3)]">
                   {isApplyPeriod
                     ? `${generation}기 모집 중!`
-                    : `${RECRUITMENT_CONFIG.openDate.month() + 1}월 중 모집 예정!`}
+                    : `${
+                        RECRUITMENT_CONFIG.openDate.month() + 1
+                      }월 중 모집 예정!`}
                   <div className="absolute -bottom-2 left-1/2 h-0 w-0 -translate-x-1/2 border-l-[8px] border-r-[8px] border-t-[8px] border-l-transparent border-r-transparent border-t-white" />
                 </div>
               </div>
